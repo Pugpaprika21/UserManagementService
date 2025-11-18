b4d7665fd332317ecc6e802f0f89801eb2505935
__Pug__ <59162541+Pugpaprika21@users.noreply.github.com>
2025-11-18 21:19:05 +0700

    UPDATE CODE

---
+++ UserManagementService/Api/Controllers/UserController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace UserManagementService.Api.Controllers
+{
+    public class UserDto { }
+
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class UserController : ControllerBase
+    {
+        [HttpGet("get/{id?}")]
+        public ActionResult<UserDto> GetDetail(int? id)
+        {
+            return Ok(new UserDto());
+        }
+    }
+}
--- UserManagementService/Program.cs
+++ UserManagementService/Program.cs
@@ -1,23 +1,26 @@
+using System.Text.Json;
+
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
-// Add services to the container.
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.SnakeCaseLower;
+});
 
-builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
